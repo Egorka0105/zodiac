3d0476298c1f02177e05f06e05f2a4137f912dc9
ihorDudkevych <egorka01051993@gmail.com>
2022-08-16 23:21:50 +0300

    check

--- src/js/scriptForm.js
+++ src/js/scriptForm.js
@@ -1,31 +1,55 @@
 window.addEventListener("load", function () {
-    document.querySelector(".form-submit").addEventListener('submit', function (event) {
-        event.preventDefault();
-        const formData = new FormData(this);
-        fetch("/ajax/feedback/feedback/",
-            {
-                method: "POST",
-                body: formData,
-            })
-            .then(function (res) {
-                return res.json();
-            })
-            .then(function (data) {
-                document.querySelector(".form-submit").reset();
 
-                document.querySelector(".modal-thank-title").innerHTML = data.title;
-                document.querySelector(".modal-thank-desc").innerHTML = data.desc;
-                document.querySelector(".modal-thank").classList.add('active');
-                document.querySelector(".overflow-modal").classList.add('active');
-            })
-    });
+    const form = $(".form-submit");
 
-    const formClose = document.querySelectorAll (".overflow-modal");
-    formClose.forEach(function (close) {
-        close.addEventListener('click', (event) => {
-            document.querySelector('.modal-thank').classList.remove("active");
-            document.querySelector('.overflow-modal').classList.remove("active");
-        })
-    });
+    form.addEventListener('submit',formSend);
+
+    async function formSend(e) {
+        e.preventDefault();
+        let formData = new FormData(form);
+
+        let response = await fetch('sendMail.php', {
+            method:"POST",
+            body: formData
+        });
+        if (response.ok) {
+            let result = await response.json();
+            alert("ok");
+            form.reset();
+        } else {
+            alert("error")
+        }
+
+
+    }
+    //
+    // form.addEventListener('submit', async function (event) {
+    //     event.preventDefault();
+    //     const formData = new FormData(this);
+    //     fetch("",
+    //         {
+    //             method: "POST",
+    //             body: formData,
+    //         })
+    //         .then(function (res) {
+    //             return res.json();
+    //         })
+    //         .then(function (data) {
+    //             document.querySelector(".form-submit").reset();
+    //
+    //             document.querySelector(".modal-thank-title").innerHTML = data.title;
+    //             document.querySelector(".modal-thank-desc").innerHTML = data.desc;
+    //             document.querySelector(".modal-thank").classList.add('active');
+    //             document.querySelector(".overflow-modal").classList.add('active');
+    //         })
+    // });
+
+    // const formClose = document.querySelectorAll (".overflow-modal");
+    // formClose.forEach(function (close) {
+    //     close.addEventListener('click', (event) => {
+    //         document.querySelector('.modal-thank').classList.remove("active");
+    //         document.querySelector('.overflow-modal').classList.remove("active");
+    //     })
+    // });
 });
 
